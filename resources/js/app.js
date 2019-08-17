668b19e8dd9e5ec082ed96603002752873ed0be0
stassmoke <stanisalv.kulunych@gmail.com>
2019-08-17 16:52:04 +0300

    add page item

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,12 +17,6 @@ $(document).ready(function() {
         delay: 50,
         theme: 'tooltipster-noir'
     });
-    // $.smoothScroll({
-    //     stepSize: 1000,
-    //     accelerationDelta : 10,
-    //     animationTime    : 1000,
-    // });
-
 
     let mySwiper = new Swiper('.swiper-container', {
         speed: 400,
@@ -30,62 +24,125 @@ $(document).ready(function() {
         autoplay: true
     });
 
-});
-
-
-
-
-let $grid = $('.grid').isotope({
-    itemSelector: '.element-item, .shop-item',
-    layoutMode: 'fitRows',
-    getSortData: {
-        name: '.name',
-        symbol: '.symbol',
-        number: '.number parseInt',
-        category: '[data-category]',
-        weight: function( itemElem ) {
-            let weight = $( itemElem ).find('.weight').text();
-            return parseFloat( weight.replace( /[\(\)]/g, '') );
+// isotope grid
+    let $grid = $('.grid').isotope({
+        itemSelector: '.element-item, .shop-item',
+        layoutMode: 'fitRows',
+        getSortData: {
+            name: '.name',
+            symbol: '.symbol',
+            number: '.number parseInt',
+            category: '[data-category]',
+            weight: function( itemElem ) {
+                let weight = $( itemElem ).find('.weight').text();
+                return parseFloat( weight.replace( /[\(\)]/g, '') );
+            }
         }
-    }
-});
+
+    });
 
 // filter functions
-let filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function() {
-        let number = $(this).find('.number').text();
-        return parseInt( number, 10 ) > 50;
-    },
-    // show if name ends with -ium
-    ium: function() {
-        let name = $(this).find('.name').text();
-        return name.match( /ium$/ );
-    }
-};
+    let filterFns = {
+        // show if number is greater than 50
+        numberGreaterThan50: function() {
+            let number = $(this).find('.number').text();
+            return parseInt( number, 10 ) > 50;
+        },
+        // show if name ends with -ium
+        ium: function() {
+            let name = $(this).find('.name').text();
+            return name.match( /ium$/ );
+        }
+    };
 
 // bind filter button click
-$('#filters').on( 'click', 'button', function() {
-    let filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $grid.isotope({ filter: filterValue });
-});
+    $('#filters').on( 'click', 'button', function() {
+        let filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
 
 // bind sort button click
-$('#sorts').on( 'click', 'button', function() {
-    let sortByValue = $(this).attr('data-sort-by');
-    $grid.isotope({ sortBy: sortByValue });
-});
+    $('#sorts').on( 'click', 'button', function() {
+        let sortByValue = $(this).attr('data-sort-by');
+        $grid.isotope({ sortBy: sortByValue });
+    });
 
 // change is-checked class on buttons
-$('.filters-buttons').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $( this ).addClass('is-checked');
+    $('.filters-buttons').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+        });
     });
+
+// tabs
+    $("ul.tabs__caption").on("click", "li:not(.active)", function () {
+        $(this)
+            .addClass("active")
+            .siblings()
+            .removeClass("active")
+            .closest("div.wrap-tabs")
+            .find("div.tabs__content")
+            .removeClass("active")
+            .eq($(this).index())
+            .addClass("active")
+        ;
+
+    });
+
 });
+
+
+
+
+
+//youtube script
+    let tag = document.createElement('script');
+        tag.src = "//www.youtube.com/iframe_api";
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    let player;
+
+    let dataSrc = document.getElementById("player").getAttribute("data-id");
+
+    window.onYouTubeIframeAPIReady = function () {
+        player = new YT.Player('player', {
+            height: '100%',
+            width: '100%',
+            videoId: dataSrc,  // youtube video id
+            playerVars: {
+                'autoplay': 0,
+                'rel': 0,
+                'showinfo': 0
+            },
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    };
+
+    var p = document.getElementById ("player");
+    $(p).hide();
+
+    var t = document.getElementById ("thumbnail");
+    t.src = t.getAttribute("data-src");
+
+    window.onPlayerStateChange = function (event) {
+        if (event.data == YT.PlayerState.ENDED) {
+            $('.start-video').fadeIn('normal');
+        }
+    };
+
+    $(document).on('click', '.start-video', function () {
+        $(this).hide();
+        $("#player").show();
+        $("#thumbnail_container").hide();
+        player.playVideo();
+    });
 
 
 
--- resources/views/page_item.blade.php
+++ resources/views/page_item.blade.php
@@ -18,10 +18,140 @@
                 <div class="row-sides">
                     <div class="l-side">
                         <div class="wrap-video">
-                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/GOp3Nhzfr-I?controls=0"
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                    allowfullscreen></iframe>
+                            <div id="player" data-id="GOp3Nhzfr-I"></div>
+                            <div id="thumbnail_container" class="thumbnail_container">
+                                <img class="thumbnail" id="thumbnail"
+                                     data-src="/img/background/Creative Slideshow Preview Image.jpg"/>
+                            </div>
+
+                            <a class="start-video">
+                                <img class="preview-img" width="64"
+                                     src="http://image.flaticon.com/icons/png/512/0/375.png"
+                                     style="filter: invert(100%); -webkit-filter: invert(100%);">
+                            </a>
+                        </div>
+                        <div class="wrap-tabs">
+                            <ul class="tabs__caption">
+                                <li class="active">Item Details</li>
+                                <li>Comments</li>
+                                <li>Reviews</li>
+                                <li>Support</li>
+                                <li>item FAQ</li>
+                            </ul>
+                            <div class="wrap-tab__content">
+                                <div class="tabs__content active">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Haec quo modo
+                                        conveniant, non sane intellego. Hanc ergo intuens debet institutum illud quasi
+                                        signum absolvere. Et ille ridens: Video, inquit, quid agas; Duo Reges:
+                                        constructio interrete. Ad eos igitur converte te, quaeso. Atque haec ita
+                                        iustitiae propria sunt, ut sint virtutum reliquarum communia.
+                                    </p>
+                                    <p>
+                                        It is a long established fact that a reader will be distracted by the readable
+                                        content of a page when looking at its layout. The point of using Lorem Ipsum is
+                                        that it has a more-or-less normal distribution of letters, as opposed to using
+                                        ‘Content here, content here’, making it look like readable English. Many desktop
+                                        publishing packages and web page editors now use Lorem Ipsum as their default
+                                        model text, and a search for ‘lorem ipsum’ will uncover many web sites still in
+                                        their infancy. Various versions have evolved over the years, sometimes by
+                                        accident, sometimes on purpose.
+                                    </p>
+                                    <h4>
+                                        Features
+                                    </h4>
+                                    <ul>
+                                        <li>
+                                            Software – WordPress
+                                        </li>
+                                        <li>
+                                            Compatible Browsers – IE9+, Chrome, Safari, Opera, Firefox
+                                        </li>
+                                        <li>
+                                            Documentation – Yes
+                                        </li>
+                                        <li>
+                                            License – GPL
+                                        </li>
+                                        <li>
+                                            Version – 1.0.1
+                                        </li>
+                                        <li>
+                                            Layout – Responsive
+                                        </li>
+                                        <li>
+                                            Columns – 4
+                                        </li>
+                                        <li>
+                                            Files Included – php, html, JS, css
+                                        </li>
+                                        <li>
+                                            Compatible with – Elementor
+                                        </li>
+                                    </ul>
+                                    <div>
+                                        <p>
+                                            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below
+                                            for those interested. Sections 1.10.32 and 1.10.33 from “de Finibus Bonorum
+                                            et Malorum” by Cicero are also reproduced in their exact original form,
+                                            accompanied by English versions from the 1914 translation by H. Rackham.
+                                        </p>
+                                        <h4>
+                                            Sources and Credits:
+                                        </h4>
+                                        <ul>
+                                            <li>
+                                                <a href="#">Bootstrap v4.0.0</a>
+                                            </li>
+                                            <li><a href="#">
+                                                    jQuery v3.3.1
+                                                </a></li>
+                                            <li><a href="#">
+                                                    Animate CSS
+                                                </a></li>
+                                            <li><a href="#">
+                                                    FontAwesome
+                                                </a></li>
+                                            <li><a href="#">
+                                                    Smoothscroll
+                                                </a></li>
+                                            <li><a href="#">
+                                                    Parallax
+                                                </a></li>
+                                            <li><a href="#">
+                                                    Owl carosel
+                                                </a></li>
+                                            <li><a href="#">
+                                                    isotop
+                                                </a></li>
+                                            <li><a href="#">
+                                                    Waypoints
+                                                </a></li>
+                                            <li><a href="#">
+                                                    Countdown
+                                                </a></li>
+                                            <li><a href="#">
+                                                    sPreoader
+                                                </a></li>
+                                        </ul>
+                                        <strong>
+                                            Note: Images are not included with the main files. They are used for preview purposes only
+                                        </strong>
+                                    </div>
+                                </div>
+                                <div class="tabs__content">
+                                    Comments
+                                </div>
+                                <div class="tabs__content">
+                                    Reviews
+                                </div>
+                                <div class="tabs__content">
+                                    Support
+                                </div>
+                                <div class="tabs__content">
+                                    item FAQ
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="r-side">
